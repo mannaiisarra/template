e181d28070160a041830c1f3fc47fb5de63592ad
mannaisarra <sarra.mannai@esprit.tn>
2022-05-24 01:26:40 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './home/dashboard/dashboard.component';
+import { HeaderComponent } from './home/header/header.component';
+import { SidebarComponent } from './home/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
